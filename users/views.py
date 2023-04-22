49f5d784ccb1205f91eee3ffc665faa453a41ab3
samatov914 <samatov914@gmail.com>
2023-04-22 15:48:05 +0600

    hw7

--- users/views.py
+++ users/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 from users.forms import UserRegistrationForm
 
-# Create your views here.
 
-def register(request):
-    if request.method == "POST":
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.clean_password2())
-            new_user.save()
-            return render(request, "registration/register_done.html", {'user' : new_user})
-    
-    else:
-        user_form = UserRegistrationForm()
-    return render(request, 'registration/register.html', {"form" : user_form})
+
+class RegisterView(generic.FormView):
+    form_class = UserRegistrationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        return response
